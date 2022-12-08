e6013ebe6bb529249695c6871a482d45395c9fa4
chenzhiqiao <13030206192@163.com>
2022-12-09 00:45:24 +0800

    提交

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,5 @@
 import { lazy } from 'react'
-// 基于路由进行代码分割     
-// 经测试可进行路由懒加载
-// https://zh-hans.reactjs.org/docs/code-splitting.html#route-based-code-splitting
+
 const router = [
 	{
 		path: "/user",
